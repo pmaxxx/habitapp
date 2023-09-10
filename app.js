2d804247cf5abc468c4edeae4f4f7a1f5747d13b
pmaxxx <maxim.putin@gmail.com>
2023-09-10 20:25:48 +0500

    app update

--- app.js
+++ app.js
@@ -6,37 +6,58 @@ const habits = [];
 function addHabit(e) {
     e.preventDefault();
     const text = this.querySelector('[name=habit]').value;
-    const totalCounts = this.querySelector('[name=reps').value;
+    const totalCounts = +this.querySelector('[name=reps').value;
     const timeframe = this.querySelector('[name=timeframe').value;
     const habit = {
         text: text,
         reps: 0,
         totalCounts: totalCounts,
         timeframe: timeframe,
-        complited: false,
+        completed: false,
     };
 
     habits.push(habit);
+    this.reset();
+    listHabits(habits, habitsList);
 }
 
 // List Habit
 function listHabits(habit = [], habitsList) {
-    habitsList.innerHTML = habits.map((habit, i) => {
-        return `
+    habitsList.innerHTML = habits
+        .map((habit, i) => {
+            return `
 		 <li>
 		 <input type="checkbox" data-index=${i} id="habit${i}" ${
-            habit.complited ? 'checked' : ''
-        } />
-		 <label for="habit${i}">${habit.reps}/${habit.totalCounts} ${
-            habit.timeframe
-        } <span>${habit.text}</span></label>
+                habit.completed ? 'checked' : ''
+            } />
+		 <label for="habit${i}"><span>${habit.reps}/${habit.totalCounts} ${
+                habit.timeframe
+            }</span> ${habit.text}</label>
 		 </li>
 		`;
-    });
+        })
+        .join('');
 }
 // Toggle if complite
+function toggleCompleted(e) {
+    console.log(e.target);
+    if (!e.target.matches('input')) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    habits[index].reps += 1;
+
+    if (habits[index].reps === habits[index].totalCounts) {
+        habits[index].completed = true;
+    } else if (habits[index].reps > habits[index].totalCounts) {
+        habits[index].reps = 0;
+        habits[index].completed = false;
+    }
+
+    listHabits(habits, habitsList);
+}
 
 // Delete habit
 
 addHabits.addEventListener('submit', addHabit);
+habitsList.addEventListener('click', toggleCompleted);
 // listHabits(habits, habitsList);
